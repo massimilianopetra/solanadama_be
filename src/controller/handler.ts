e5d5d27193dc38cd40734099e885c0789e5ba8f6
Massimiliano Petra <massimiliano.petra@gmail.com>
2024-06-04 11:39:27 +0200

    query test!

--- src/controller/handler.ts
+++ src/controller/handler.ts
@@ -1,19 +1,37 @@
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
+import mariadb from 'mariadb';
 import log4js from "log4js";
 
-export function connectHandler(req: Request, res: Response){
+export function connectHandler(req: Request, res: Response) {
     const logger = log4js.getLogger("dama");
     logger.info("POST /connect");
     logger.info(`Wallet Adapter Name: ${req.body.adapter}`);
-    res.json({outcome:true});
+    res.json({ outcome: true });
 }
 
-export function sendEmailHandler(req: Request, res: Response){
+export function sendEmailHandler(req: Request, res: Response) {
     const logger = log4js.getLogger("dama");
     logger.info("POST /sendemail");
     logger.info(`Message From: ${req.body.email}`);
     logger.info(`Message Subject: ${req.body.subject}`);
     logger.info(`Message: ${req.body.message}`);
-    
-    res.json({outcome:true});
+
+    res.json({ outcome: true });
+
+    const pool = mariadb.createPool({
+        host: process.env.db_host,
+        user: process.env.db_username,
+        password: process.env.db_password,
+        database: process.env.db_database,
+        connectionLimit: 5
+    });
+    pool.getConnection()
+        .then(conn => {
+            conn.query("SELECT * from Mail;")
+            .then((rows) => {
+                console.log(rows);
+            });
+        }).catch(err => {
+            //not connected
+        });
 }
