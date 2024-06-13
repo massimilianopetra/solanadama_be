7b5d58e1d4c65e4d500920722e3c8d69e108da1a
Massimiliano Petra <massimiliano.petra@gmail.com>
2024-06-13 15:58:17 +0200

    added transaction api

--- src/controller/solanaHandler.ts
+++ src/controller/solanaHandler.ts
@@ -4,10 +4,81 @@ import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/s
 import { clusterApiUrl, Connection, Keypair, PublicKey, Transaction, sendAndConfirmTransaction } from '@solana/web3.js';
 import { createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
 import * as fs from 'fs';
+import bs58 from 'bs58';
 
 export function mintTokenHandler(req: Request, res: Response){
     const logger = log4js.getLogger("dama");
     logger.info("POST /minttoken");
 
     res.json({outcome:true});
+}
+
+export function sendTransaction(req: Request, res: Response){
+    const logger = log4js.getLogger("dama");
+    logger.info("POST /sendtransaction");
+}
+
+export function sendTrasactionDevnet(req: Request, res: Response) {
+    const logger = log4js.getLogger("dama");
+    logger.info("POST /sendtransactiondevnet");
+
+    const connection = new Connection(clusterApiUrl("devnet"));
+
+    const txBS58:string = req.body.params[0];
+
+    // txBS58 is truthy strValue was non-empty string, true, 42, Infinity, [],
+    if (txBS58) {
+        const txUint8Array = bs58.decode(txBS58);
+        const txid = connection.sendRawTransaction(txUint8Array);
+        logger.info(`txid = ${txid}`);
+        res.json({
+            "jsonrpc": "2.0",
+            "result": txid,
+            "id": 1
+          });
+    }
+    
+    res.json({ outcome: "ERROR" });
+}
+
+export async function sendTrasactionMainnet(req: Request, res: Response) {
+    const logger = log4js.getLogger("dama");
+    logger.info("POST /sendtransaction");
+
+    const connection = new Connection(clusterApiUrl("mainnet-beta"));
+
+    const txBS58:string = req.body.params[0];
+
+    // txBS58 is truthy strValue was non-empty string, true, 42, Infinity, [],
+    if (txBS58) {
+        const txUint8Array = bs58.decode(txBS58);
+        const txid = await connection.sendRawTransaction(txUint8Array);
+        logger.info(`txid = ${txid}`);
+        res.json({
+            "jsonrpc": "2.0",
+            "result": txid,
+            "id": 1
+          });
+    }
+    
+    res.json({ outcome: "ERROR" });
+}
+
+export async function confirmTrasactionMainnet(req: Request, res: Response) {
+    const logger = log4js.getLogger("dama");
+    logger.info("POST /confirmtransaction");
+
+    const connection = new Connection(clusterApiUrl("mainnet-beta"));
+
+    const txid:string = req.body.resut;
+
+    // txid is truthy strValue was non-empty string, true, 42, Infinity, [],
+    if (txid) {
+
+        logger.info(`txid = ${txid}`);
+        res.json({ outcome: "OK" });
+
+    }
+    
+    res.json({ outcome: "ERROR" });
 }
